5d463dbb320530eca237d78199fa8899e27cadb4
Noel9356 <noel.poetica.caelus@gmail.com>
2026-03-11 23:48:23 +0700

    Fix capacity counting error.

--- obj/Debug/net10.0/webapp_miniproject.AssemblyInfo.cs
+++ obj/Debug/net10.0/webapp_miniproject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapp_miniproject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97290736c33fdc7577915ba87d1491dcf4bc20c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc7722829207220e008f761801a0355fe7bc6e68")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapp_miniproject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapp_miniproject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
