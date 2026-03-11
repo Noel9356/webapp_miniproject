9be0b0874d75eb41a980f5a210b55546bfe9056f
R'Heng!!! <chawanwit9899@gmail.com>
2026-03-11 22:27:44 +0700

    ...

--- obj/Debug/net10.0/webapp_miniproject.AssemblyInfo.cs
+++ obj/Debug/net10.0/webapp_miniproject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapp_miniproject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48880fc8961cac3eb2a44ce42add0827338894f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf6fd2f74b6901c60ccefb9ee9e245defaeab4ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapp_miniproject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapp_miniproject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
